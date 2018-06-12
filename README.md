836c4f8423b78d4c0d4de223a4c08ca2af7e2946
Dwalton55 <deonte.e.walton@gmail.com>
2018-06-12 12:35:10 -0400

    updated readme with required information

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Project2-BJJ
+Project 2 Overview For project #2, I've created an app designed to track techniques and progress in jiujitsu.
+this is what one would call their jiujitsu game plan.
+
+the site is a test site based on a coach or a school view.  the school personel will be able to see different students/ users and their game plans. the school can then add to that users information/gameplans/and techniques. I have alot of plans for future add ons but have yet to begin implementing these.
+
+GitHub: https://github.com/Dwalton55/Project2-BJJ
+Trello:	https://trello.com/b/xamudaWe/project-2-bjj-app
+Heroku: https://vast-thicket-41925.herokuapp.com/user
+
+Technologies Used Languages -  
+Express - 
+Node    - 
+Project Planning & User Stories
+Trello  -
+Visual Code -
+
+ Features: the features so far include routing and editing different aspects of the user and game plan. 
+Future Development: 
+add both a coach and student view. 
+attendance tracker
+goals tracker
+video upload and download
+comments
+training plans
+scheduling
+payment plans
+school selector.
--- routes/technique.js
+++ routes/technique.js
@@ -6,108 +6,112 @@ const Technique = require('../models/technique')
 
 //index
 router.get('/', (req, res) => {
-    const userid = req.params.id
-    const gameplan = req.params.gameid
-    const positionid =req.params.posid
-    User.findById(userid)
+  const userid = req.params.id
+  const gameplan = req.params.gameid
+  const positionid = req.params.posid
+  User.findById(userid)
     .then((userGP) => {
       const gameplans = userGP.gamePlans.id(gameplan)[positionid]
       console.log(gameplans)
-    //   console.log(gameplans)
+      //   console.log(gameplans)
       res.render('user/gameplan/techniques/techniqueIndex', {
         userid,
         gameplans,
         positionid
-        
+
 
       })
     })
-  })
+})
 //new
 router.get('/new', (req, res) => {
-    
-    res.render('user/gameplan/techniques/techniqueNew', {
-      position: req.params.posid,
-      userID: req.params.id,
-      gameplan: req.params.gameid,
-      positionid: req.params.posid,
-      
-    })
+
+  res.render('user/gameplan/techniques/techniqueNew', {
+    position: req.params.posid,
+    userID: req.params.id,
+    gameplan: req.params.gameid,
+    positionid: req.params.posid,
+
   })
+})
 //creat
 router.post('/', (req, res) => {
 
-    // make comment req.body
-    const technique = new Technique(req.body)
-    const userid = req.params.id
-    const gameplan = req.params.gameid
-    const positionid =req.params.posid
-    // get user by the id
-    User.findById(req.params.id)
-      .then((userID) => {
-  
-        // push new gameplan to gamePlans
-        userID.gamePlans.id(gameplan)[positionid].techniques.push(technique)
-        console.log(userID + "test userid")
-        console.log(userID.gameplans + "test gameplans")
-        console.log(positionid + "test posid")
-        console.log(positionid.techniques + "test techniques")
+  // make comment req.body
+  const technique = new Technique(req.body)
+  const userid = req.params.id
+  const gameplan = req.params.gameid
+  const positionid = req.params.posid
+  // get user by the id
+  User.findById(req.params.id)
+    .then((userID) => {
 
-        // save the gameplan
-        return userID.save()
-      })
-      .then(() => {
-  
-        // redirect to gameplans
-        // /user/:id/gameplans/:gameid/:posid
-        res.redirect(`/user/${req.params.id}/gameplans/${req.params.gameid}/${req.params.posid}`)
-      })
-  })
+      // push new gameplan to gamePlans
+      userID.gamePlans.id(gameplan)[positionid].techniques.push(technique)
+      console.log(userID + "test userid")
+      console.log(userID.gameplans + "test gameplans")
+      console.log(positionid + "test posid")
+      console.log(positionid.techniques + "test techniques")
+
+      // save the gameplan
+      return userID.save()
+    })
+    .then(() => {
+
+      // redirect to gameplans
+      // /user/:id/gameplans/:gameid/:posid
+      res.redirect(`/user/${req.params.id}/gameplans/${req.params.gameid}/${req.params.posid}`)
+    })
+})
 //edit
 router.get('/:techid/edit', (req, res) => {
 
-    const techid = req.params.techid
-    const gameplan = req.params.gameid
-    const positionid =req.params.posid
-    User
-      .findById(req.params.id)
-      .then((technique) => {
-        res.render('user/gameplan/techniques/techniqueEdit', { 
-            techid,
-            technique,
-            gameplan,
-            positionid,
-        
-        })
+  const techid = req.params.techid
+  const gameplan = req.params.gameid
+  const positionid = req.params.posid
+  User
+    .findById(req.params.id)
+    .then((technique) => {
+      res.render('user/gameplan/techniques/techniqueEdit', {
+        techid,
+        technique,
+        gameplan,
+        positionid,
+
       })
-  })
+    })
+})
 
 //update
 
 router.put('/:techid', (req, res) => {
-    const gameplan = req.params.gameid
-    const positionid =req.params.posid
-    const techid = req.params.techid
-    const update = req.body
-    User.findById(req.params.id)
+  const gameid = req.params.gameid
+  const positionid = req.params.posid
+  const techid = req.params.techid
+  const update = req.body
+  User.findById(req.params.id)
     .then((user) => {
-        const tech = user.gamePlans.id(gameid)[positionid].techniques.techid
-        tech.Name = update.Name
-        tech.Description = update.Description
+      const tech = user.gamePlans.id(gameid)[positionid].techniques.id(techid)
+      console.log(user.gamePlans.id(gameid)[positionid].techniques)
+      console.log(user.gamePlans.id(gameid)[positionid].techniques.id(techid))
+      tech.Name = update.Name
+      tech.Description = update.Description
+      return user.save()
 
-        return user.save()
-
+    }).then(user => {
+      res.redirect(`/user/${user._id}/gameplans/${gameid}/${positionid}`)
     })
-    res.redirect(`/user/${User._id}/gameplans/${gameplan}/${positionid}`)
-  })
+
   
+})
+
 //delete
 router.delete('/:id', (req, res) => {
-    techniques.findByIdAndRemove(req.params.posid)
-      .then(() => {
-        console.log('Successfully Delete ')
-        res.redirect('/user/:id/gameplans/:gameid/:posid')
-      })
-  })
+  User.findByIdAndRemove(req.params.posid)
+    .then(() => {
+      console.log('Successfully Delete ')
+      res.redirect('/user/:id/gameplans/:gameid/:posid')
+    })
+})
 
 module.exports = router
